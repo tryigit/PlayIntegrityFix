b240fbcf717d45d8e16e982b2e969cd71b702f98
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-08-12 23:44:04 +0300

    Update ADVANCED-AllAppsTarget.sh

--- PIFB/ADVANCED-AllAppsTarget.sh
+++ PIFB/ADVANCED-AllAppsTarget.sh
@@ -1,14 +1,8 @@
 # This code adds all system apps to Magiskhide list. But I don't know how to see procs with .sh If you want to develop it, send a pull request.
 # May be incompatible with other modules!
-
-# Rename to AllAppsTarget.sh once completed.
-# Move path: /data/adb/tricky_store/AllAppsTarget.sh
+# Just run, no auto support.
 
 # Telegram: t.me/cleverestech
-
-su -c > /data/adb/tricky_store/target.txt
-
-su -c pm list packages | awk -F: '{print $2}' > /data/adb/tricky_store/target.txt
 
 packages=$(su -c "pm list packages -s | cut -d ':' -f 2") 
 
@@ -842,17 +836,3 @@ for entry in "${packages[@]}"; do
   proc=$(echo $entry | cut -d'\|' -f2)
   denylist_add "$package" "$proc"
 done
-
-# Temporarily stopped due to server need. This is torture for most people.
-
-#if [ -f "/data/adb/tricky_store/keybox.xml" ]; then
-#  rm "/data/adb/tricky_store/keybox.xml"
-#fi
-#random_keybox=$(find "/data/adb/modules/tricky_store/keyboxs/keyboxs/" -type f -name "*.xml" | shuf -n 1)
-#if [ -z "$random_keybox" ]; then
-#    exit 1
-#fi
-#cp "$random_keybox" "/data/adb/tricky_store/keybox.xml" 
-
-#su -c killall com.google.android.gms
-#su -c killall com.google.android.gms.unstable
