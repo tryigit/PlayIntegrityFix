2b6037c8902b7b26a56e19005f233093b9a33281
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-08-12 20:43:39 +0300

    Create DEFAULF-AllAppsTarget.sh

---
+++ PIFS/DEFAULF-AllAppsTarget.sh
@@ -0,0 +1,21 @@
+# Telegram: t.me/cleverestech
+
+# Create or overwrite the target.txt file
+su -c > /data/adb/tricky_store/target.txt
+
+# Use to list all packages and process the output directly to target.txt
+su -c pm list packages | awk -F: '{print $2}' > /data/adb/tricky_store/target.txt
+
+# Temporarily stopped due to server need. This is torture for most people.
+
+#if [ -f "/data/adb/tricky_store/keybox.xml" ]; then
+#  rm "/data/adb/tricky_store/keybox.xml"
+#fi
+#random_keybox=$(find "/data/adb/modules/tricky_store/keyboxs/keyboxs/" -type f -name "*.xml" | shuf -n 1)
+#if [ -z "$random_keybox" ]; then
+#    exit 1
+#fi
+#cp "$random_keybox" "/data/adb/tricky_store/keybox.xml" 
+
+#su -c killall com.google.android.gms
+#su -c killall com.google.android.gms.unstable
