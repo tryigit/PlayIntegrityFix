1aa306f70a5d40826c6693676cdab39fb36db162
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-08-13 03:49:41 +0300

    Update AllAppsTarget.sh

--- PIFB/AllAppsTarget.sh
+++ PIFB/AllAppsTarget.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+# This code adds all system apps to Magiskhide list. But I don't know how to see procs with .sh If you want to develop it, send a pull request.
+# May be incompatible with other modules!
+# Move to /data/adb/SystemAppAdd.sh and rum.
 
 # Telegram: t.me/cleverestech
 
