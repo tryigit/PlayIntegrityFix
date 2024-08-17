d86432d6521dbf9af297d4cc5f0c24690da31983
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-08-17 20:16:47 +0300

    Update DEFAULF-AllAppsTarget.sh

--- PIFS/DEFAULF-AllAppsTarget.sh
+++ PIFS/DEFAULF-AllAppsTarget.sh
@@ -3,6 +3,8 @@
 
 # Telegram: t.me/cleverestech
 
+su -c "magisk --denylist add com.google.android.gms com.google.android.gms.unstable"
+
 su -c > /data/adb/tricky_store/target.txt
 
 su -c pm list packages | awk -F: '{print $2}' > /data/adb/tricky_store/target.txt
